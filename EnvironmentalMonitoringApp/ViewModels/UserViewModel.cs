3fc6ff916a494c0bfdbffaa12bb2da19a883b666
Brody Walker <35873593+Detaaz@users.noreply.github.com>
2025-04-26 19:44:20 +0100

    Add user role management Allows an administrator to change the roles for each user.

--- EnvironmentalMonitoringApp/ViewModels/UserViewModel.cs
+++ EnvironmentalMonitoringApp/ViewModels/UserViewModel.cs
@@ -6,6 +6,7 @@ using System.ComponentModel.DataAnnotations;
 using static System.Net.Mime.MediaTypeNames;
 using Microsoft.EntityFrameworkCore;
 using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Collections.ObjectModel;
 
 
 namespace EnvironmentalMonitoringApp.ViewModels;
@@ -15,15 +16,70 @@ public partial class UserViewModel : ObservableObject, IQueryAttributable
     private Models.Users _users;
 
     public int User_id => _users.user_id;
-    public string First_name => _users.first_name;
-    public string Last_name => _users.last_name;
+    public string First_name
+    {
+        get => _users.first_name;
+        set
+        {
+            if (_users.first_name != value)
+            {
+                _users.first_name = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+    public string Last_name
+    {
+        get => _users.last_name;
+        set
+        {
+            if (_users.last_name != value)
+            {
+                _users.last_name = value;
+                OnPropertyChanged();
+            }
+        }
+    }
     public string Email => _users.email;
-    public string Role => _users.role;
+    public string Role
+    {
+        get => _users.role;
+        set
+        {
+            if (_users.role != value)
+            {
+                _users.role = value;
+                OnPropertyChanged();
+            }
+        }
+    }
     public int Login_id => _users.login_id;
 
+    public string SelectedRole
+    {
+        get => _users.role;
+        set
+        {
+            if (_users.role != value)
+            {
+                _users.role = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+    // Combine names for presentation
     public string FullName => _users.first_name + " " + _users.last_name;
 
     private EnvMonitorDbContext _context;
+
+    public ObservableCollection<string> Roles { get; } = new ObservableCollection<string>
+    {
+        "Administrator",
+        "Operations Manager",
+        "Environmental Scientist"
+    };
+
     public UserViewModel(EnvMonitorDbContext envMonitorDbContext)
     {
         _context = envMonitorDbContext;
@@ -33,6 +89,14 @@ public partial class UserViewModel : ObservableObject, IQueryAttributable
     {
         _context = envMonitorDbContext;
         _users = user;
+    }
+
+    [RelayCommand]
+    private async Task Save()
+    {
+        _users.role = SelectedRole;
+        _context.SaveChanges();
+        await Shell.Current.GoToAsync(nameof(Views.AdminDashboard));
     }
 
     void IQueryAttributable.ApplyQueryAttributes(IDictionary<string, object> query)
@@ -53,5 +117,9 @@ public partial class UserViewModel : ObservableObject, IQueryAttributable
     private void RefreshProperties()
     {
         OnPropertyChanged(nameof(Role));
+        OnPropertyChanged(nameof(SelectedRole));
+        OnPropertyChanged(nameof(First_name));
+        OnPropertyChanged(nameof(Last_name));
+        OnPropertyChanged(nameof(FullName));
     }
 }
